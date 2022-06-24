debfa7e2a72dbcf38baf78d80fda95a966642d0d
Fynn Raddatz <fynn.raddatz@gmail.com>
2022-06-24 07:17:59 +0200

    Initial Commit

---
+++ src/Printer.cpp
@@ -0,0 +1,50 @@
+#include "Printer.hpp"
+
+
+PRINTER::PRINTER(UART_HandleTypeDef* huart)
+{
+	_huart = huart;
+}
+
+void PRINTER::printFloat(float value)
+{
+#ifdef PRINTER_DEBUG
+
+
+	__disable_irq();
+	char temp[25];
+	uint8_t data[25];
+
+	int cnt = sprintf(temp, "%f", value);
+
+	for(int i = 0; i < cnt + 1; i++)
+	{
+		data[i] = temp[i];
+	}
+
+	HAL_UART_Transmit(_huart, data, cnt, 100);
+	println();
+	__enable_irq();
+#endif
+}
+
+void PRINTER::printString(std::string str)
+{
+#ifdef PRINTER_DEBUG
+	__disable_irq();
+	std::vector<uint8_t> vec(str.begin(), str.end());
+	uint8_t *data = &vec[0];
+	//data[str.length()] = '\0';
+
+	HAL_UART_Transmit(_huart, data, str.length(), 100);
+	println();
+	__enable_irq();
+#endif
+}
+
+void PRINTER::println()
+{
+	uint8_t nl[1];
+	nl[0] = '\n';
+	HAL_UART_Transmit(_huart, nl, 1, 100);
+}
